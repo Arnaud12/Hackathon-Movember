7934b68ffd7a5efcc2953edd227d0d85514d577d
Jipsian <sylvain.labarre@live.fr>
2016-11-09 15:10:09 +0100

    préparation de la BDD

---
+++ server/api/models/photo.js
@@ -0,0 +1,73 @@
+import mongoose from 'mongoose';
+
+const photosSchema = new mongoose.Schema({
+    stachePhoto: String,
+    pseudoPhoto: String,
+    lieuPhoto: String
+});
+
+let model = mongoose.model('Photo', photosSchema);
+
+export default class Photo {
+
+    findAll(req, res) {
+        model.find({}, (err, photos) => {
+            if (err) {
+                res.sendStatus(403);
+            } else {
+                res.json(photos);
+            }
+        });
+    }
+
+    findById(req, res) {
+        model.findById(req.params.id, (err, photo) => {
+            if (err || !photo) {
+                res.sendStatus(403);
+            } else {
+                res.json(photo);
+            }
+        });
+    }
+
+    create(req, res) {
+        model.create({
+                stachePhoto: req.body.stachePhoto,
+                photoPhoto: req.body.pseudoPhoto,
+                lieuPhoto: req.body.lieuPhoto
+            },
+            (err, photo) => {
+                if (err) {
+                    res.status(500).send(err.message);
+                } else {
+                    res.json(photo);
+                }
+            });
+    }
+
+    update(req, res) {
+        model.update({
+            _id: req.params.id
+        }, {
+            stachePhoto: req.body.stachePhoto,
+            pseudoPhoto: req.body.pseudoPhoto,
+            lieuPhoto: req.body.lieuPhoto
+        }, (err, photo) => {
+            if (err || !photo) {
+                res.status(500).send(err.message);
+            } else {
+                res.json(photo);
+            }
+        });
+    }
+
+    delete(req, res) {
+        model.findByIdAndRemove(req.params.id, (err) => {
+            if (err) {
+                res.status(500).send(err.message);
+            } else {
+                res.sendStatus(200);
+            }
+        });
+    }
+}
